c29da1cc15edb205efb828430df9042ce920d723
Federico Mammana <fedmammana98gmail.com>
2021-12-16 12:12:22 +0100

    -Updated widgets -Tried (and failed) to include reactiveness of outputs to widgets -Included notes for next steps

--- Globalization_db/app.R
+++ Globalization_db/app.R
@@ -7,28 +7,53 @@ library(readr)
 library(tidyverse)
 
 p_load(rio)
-KGIdata <- rio::import(file = "data_processed/KGI.Rdata")%>%
-    mutate(hover = paste0(country, "\nKGI:", KGI))
+    #data loading does not work, load from 02_build_dashboard.R
+#KGIdata <- rio::import(file = "data_processed/KGI.Rdata")%>%
+#    mutate(hover = paste0(country, "\nKGI:", KGI))
 
 # Define UI ----------------------------------------------------------------
 ui <- fluidPage(
     titlePanel("A Globalization Dashboard (1990 - 2020)"),
+    # Need to decide on the positions of outputs and widgets
     fluidRow(
         column(3,
                wellPanel(
-                   # here we can add some other commands/filters/sliders/info,
-                   # since the plot itself come swith a year slider
-               )
+                   sliderInput("year", "Year", 1990, 2020, 2020, step = 2, sep = "")),
+               wellPanel(
+                   h4("Include small countries?"),
+                   checkboxInput("small", "Countries with population less than 100.000"), value = FALSE),
+               wellPanel(
+                   h4("Top 10 globalized countries"), tableOutput("ranking"))
         ),
         column(9,
-               plotlyOutput("world_map")
+               plotlyOutput("world_map"),
+               wellPanel(textOutput("text")),
                )
         )
     )
 
 # Define Server -----------------------------------------------------------
-server <- function(input, output) {
-
+server <- function(input, output, session) {
+    
+    #small_filter <- reactive(filter(KGIdata$small=TRUE))
+    #year_filter <- reactive(filter(year==input$year))
+    
+    # Need to write correct text
+    output$text <- renderText({ 
+        "Text to very briefly explain what the index is and how it works." 
+    })
+    
+    # Need to implement filters for year and small countries
+    output$ranking <- renderTable({
+        KGIdata %>% 
+            select(country, KGI) %>%
+            arrange(desc(KGI)) %>% 
+            head(10)
+    })
+    
+    # Need to implement filter for year and small countries
+    # Need to substitute the year slider below with a proper widget
+    # Need to fix the positioning of the map (larger and less movable)
     output$world_map <- renderPlotly({
         # Define plotly map's properties, font, labels, and layout
         graph_properties <- list(
